159f5b8d7c4ede9a2e25e0ab3e87f7f1a5604056
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-28 12:51:30 +0330

    animation in button added

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => {
      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
             <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a junior Front End Developer</h2>
-            <p className='text-gray-500 py-4 max-w-md'>I started web app design journey about 1 year ago.
+            <p className='text-gray-500 py-4 max-w-md'>I started My web app design journey about 1 year ago.
                 Currently, I love to work on web using technologies like 
-                React, Tailwind, Next.
+                React, Tailwind, Next JS and learn whatever it'll be needed.
             </p>
             <div>
-                <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                <button className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                     Portfolio
-                    <span>
-                        <IoIosArrowForward />
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <IoIosArrowForward size={20} className='ml-1'/>
                     </span>
                 </button>
             </div>
