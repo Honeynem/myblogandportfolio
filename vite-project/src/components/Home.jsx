42a7fbe6d5c59bfafb17a36f7e26766873f6e4a2
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-28 12:56:34 +0330

    minor changes done

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -9,9 +9,7 @@ const Home = () => {
      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
             <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a junior Front End Developer</h2>
-            <p className='text-gray-500 py-4 max-w-md'>I started My web app design journey about 1 year ago.
-                Currently, I love to work on web using technologies like 
-                React, Tailwind, Next JS and learn whatever it'll be needed.
+            <p className='text-gray-500 py-4 max-w-md'>I began my web design journey a year ago and enjoy working with technologies like React, Tailwind, and Next.js while staying open to learning new things.
             </p>
             <div>
                 <button className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
