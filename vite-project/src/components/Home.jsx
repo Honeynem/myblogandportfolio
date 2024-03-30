a10a6e5623bd3a5a020af4a41f551dcb5b3f318d
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-30 21:34:10 +0330

    react scroll added

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroImage from '../assets/heroImage.png';
 import { IoIosArrowForward } from "react-icons/io";
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -12,12 +13,12 @@ const Home = () => {
             <p className='text-gray-500 py-4 max-w-md'>I began my web design journey a year ago and enjoy working with technologies like React, Tailwind, and Next.js while staying open to learning new things.
             </p>
             <div>
-                <button className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                <Link to='portfolio' smooth duration={500} className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                     Portfolio
                     <span className='group-hover:rotate-90 duration-300'>
                         <IoIosArrowForward size={20} className='ml-1'/>
                     </span>
-                </button>
+                </Link>
             </div>
         </div>
         <div>
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
 
@@ -38,7 +39,9 @@ const Navbar = () => {
         </div>
 
         <ul className='hidden md:flex'>
-            {links.map(({id, link}) => (<li key={id} className='px-4 capitalize cursor-pointer text-gray-400 text-medium hover:scale-105 duration-200'>{link}</li>))}
+            {links.map(({id, link}) => (<li key={id} className='px-4 capitalize cursor-pointer text-gray-400 text-medium hover:scale-105 duration-200'>
+            <Link to={link} smooth duration={500}>{link}</Link>
+            </li>))}
             
         </ul>
 
@@ -51,7 +54,9 @@ const Navbar = () => {
         {nav && (
 
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-500 text-gray-400'>
-        {links.map(({id, link}) => (<li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{link}</li>))}
+        {links.map(({id, link}) => (<li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
+        <Link onClick={()=> setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
+        </li>))}
 
         </ul>)
         }
