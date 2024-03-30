444db296736ee6f85de6bfa9ccfad7d1424f8521
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-03-30 21:15:21 +0330

    getform added

--- vite-project/src/components/Contact.jsx
+++ vite-project/src/components/Contact.jsx
@@ -9,10 +9,22 @@ const Contact = () => {
             <p className='py-6'>Let's get in touch!</p>
         </div>
         <div className='flex justify-center items-center'>
-            <form action="" className='w-full flex flex-col md:w-1/2'>
-                <input type="text" name='name' placeholder='Enter Your Name' className='p-2 bg-transparent text-white border-2 rounded-md focus:outline-none' />
-                <input type="text" name='email' placeholder='Enter Your Email' className='p-2 bg-transparent text-white border-2 rounded-md focus:outline-none' />
-                <textarea name="message" rows="10" className='p-2 bg-transparent text-white border-2 rounded-md focus:outline-none'></textarea>
+            <form action="https://getform.io/f/paygkvxa" method='POST' className='w-full flex flex-col md:w-1/2'>
+                <input 
+                type="text" 
+                name='name' 
+                placeholder='Enter Your Name' 
+                className='p-2 bg-transparent text-white border-2 rounded-md focus:outline-none' />
+                <input 
+                type="text" 
+                name='email' 
+                placeholder='Enter Your Email'
+                className='my-4 p-2 bg-transparent text-white border-2 rounded-md focus:outline-none' />
+                <textarea 
+                name="message" 
+                placeholder='Enter Your Message' 
+                rows="10" 
+                className='p-2 bg-transparent text-white border-2 rounded-md focus:outline-none'></textarea>
                 <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Submit</button>
             </form>
 
