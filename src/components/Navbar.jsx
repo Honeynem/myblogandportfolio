4e983215b87f20bd13fc42e1ed734ec3397e21e5
honey <148876783+Honeynem@users.noreply.github.com>
2025-01-21 00:08:22 +0330

    text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
         </div>
 
         <ul className='hidden md:flex'>
-            {links.map(({id, link}) => (<li key={id} className='px-4 capitalize cursor-pointer text-gray-400 text-medium hover:scale-105 duration-200'>
+            {links.map(({id, link}) => (<li key={id} className='px-4 capitalize cursor-pointer text-[#2C3E50] text-medium hover:scale-105 duration-200'>
             <Link to={link} smooth duration={500}>{link}</Link>
             </li>))}
             
@@ -47,13 +47,13 @@ const Navbar = () => {
 
 
         {/* mobile and smaller screen menu */}
-        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-400  md:hidden'>
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-[#2C3E50]  md:hidden'>
             {nav ? <FaTimes size={30} />: <FaBars size={30} />}
         </div>
 
         {nav && (
 
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-500 text-gray-400'>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-[#DFF5E3] from-black to-gray-500 text-[#2C3E50]'>
         {links.map(({id, link}) => (<li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
         <Link onClick={()=> setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
         </li>))}
