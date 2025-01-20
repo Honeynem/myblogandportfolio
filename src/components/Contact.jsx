f7b1ca1f6d6f2b376347a1e46a3b1006818c44e3
honey <148876783+Honeynem@users.noreply.github.com>
2025-01-20 23:01:18 +0330

    c page bg to green

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
+    <div name='contact' className='w-full h-screen bg-[#DFF5E3] from-black to-gray-800 p-4 text-white'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full overflow-y-auto'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
