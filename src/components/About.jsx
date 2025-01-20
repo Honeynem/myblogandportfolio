f72fff3e5a2853bdd6a617df4c844abb9035e455
honey <148876783+Honeynem@users.noreply.github.com>
2025-01-20 16:13:43 +0330

    about me text chnaged

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,24 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full min-h-screen bg-gradient-to-w from-gray-800 to-black text-black'>
+    <div name='about' className='w-full min-h-screen bg-gradient-to-w from-gray-800 to-black text-'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full overflow-y-auto'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
 
-        <p className='mt-20 text-xl'>I build beautiful and functional web experiences.
-            I'm a passionate front-end developer with a drive to create innovative and user-friendly interfaces. I'm constantly pushing myself to learn new technologies and stay ahead of the curve. Currently, I'm diving deep into the world of <span className='font-bold'>Tailwind CSS, React, TypeScript, and Next.js</span>, building interactive and performant web applications.
+        <p className='mt-20 text-xl'>Although I really don’t want to have an "About Me" section (it always feels a bit too self-absorbed), I guess I have no choice but to share a bit about myself! 
         </p>
         <br />
         <p className='text-xl'>
 
-            My ultimate goal is to land a position on a collaborative and innovative team, where I can collaborate with talented developers and contribute to solving complex problems. My eagerness to learn extends beyond code. I'm constantly expanding my knowledge of design principles and user experience (UX) best practices, striving to create a seamless experience for users.
+        I’m a learning enthusiast—a curious mind who fell in love with the web during my undergraduate years when I was first introduced to programming. But honestly, I didn’t stick with coding for too long because I found it a bit boring. That’s when I discovered the world of UI/UX, and I’ve been hooked ever since
         </p>
         <br />
         <p className='text-xl'>
 
-            Beyond the technical aspects, I bring a positive and enthusiastic attitude to thetable. I'm a team player who thrives in collaborative environments. Ifyou're looking for a front-end developer who is always learning,passionate about new technologies, and eager to contribute to asuccessful team, then let's connect!
+        Now, I’m all about creating designs that are both delightful and effortless, focusing on the Next Billion Users. I truly believe this group holds immense potential to either make or break businesses, and I’m excited to be part of that journey!
         </p>
       </div>
     </div>
