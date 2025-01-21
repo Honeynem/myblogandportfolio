abf9828107007133dc440ef72707ef3efd983aa9
honey <148876783+Honeynem@users.noreply.github.com>
2025-01-21 23:18:08 +0330

    n

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-[#DFF5E3] from-black via-black to-gray-800'>
      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#2C3E50]'>I am an Emerging Product Designer
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#2C3E50] pt-4'>I am an Emerging Product Designer
             </h2>
             <p className='text-gray-500 py-4 max-w-md'>My design journey started because I’ve always been fascinated by seeing my ideas come to life—how colors, hovers, buttons, and clean, minimal layouts can create seamless, beautiful experiences for users.
             </p>
